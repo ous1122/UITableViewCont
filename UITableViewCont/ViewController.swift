46182b068797208012731ff1eeaf55457ae289bf
D7703_24 <d7703_24@d7703-24ui-imac.local>
2018-05-16 15:52:14 +0900

    123

--- UITableViewCont/ViewController.swift
+++ UITableViewCont/ViewController.swift
@@ -1,25 +1,41 @@
-//
-//  ViewController.swift
-//  UITableViewCont
-//
-//  Created by D7703_24 on 2018. 5. 16..
-//  Copyright © 2018년 D7703_24. All rights reserved.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
-
+class MyTableViewController: UITableViewController {
+    
+    var data = ["홍길동", "임꺽정", "이순신"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    // MARK: - Table view data source
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return data.count
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "RE", for: indexPath)
+        
+        // Configure the cell...
+        cell.textLabel?.text = data[indexPath.row]
+        
+        return cell
+    }
 }
-
